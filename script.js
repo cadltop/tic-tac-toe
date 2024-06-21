6cc4495da94ce64aec96b5e9b02fbce0515c8ece
cadltop <158861368+cadltop@users.noreply.github.com>
2024-06-20 23:19:06 -0500

    Create player factory function.

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const gameBoard = (function() {
     let board = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
     return {board};
 })();
+
+function player(name, choice) {
+    return {name, choice};
+};
