0ce889e030d348a78ec1135485f09e260785a37b
cadltop <158861368+cadltop@users.noreply.github.com>
2024-06-21 23:37:16 -0500

    Create statements to place choices on the board.

--- script.js
+++ script.js
@@ -21,9 +21,21 @@ const game = (function() {
     const computer = player('Computer', computerChoice);
 
     const humanPosition = parseInt(prompt('Take a position (0-8)', ''));
-    gameBoard.board[humanPosition] = human.choice;
-    const computerPosition = Math.round((Math.random() * 7) + 1);
-    gameBoard.board[computerPosition] = computer.choice;
-
+    if(gameBoard.board[humanPosition] !== human.choice && gameBoard.board[humanPosition] !== computer.choice) {
+        gameBoard.board[humanPosition] = human.choice;
+        let computerPosition = Math.round((Math.random() * 7) + 1);
+        if(gameBoard.board[computerPosition] !== human.choice && gameBoard.board[computerPosition] !== computer.choice) {
+            gameBoard.board[computerPosition] = computer.choice;
+            console.log(gameBoard.board);
+        } else {
+            while(gameBoard.board[computerPosition] === human.choice || gameBoard.board[computerPosition] === computer.choice) {
+                computerPosition = Math.round((Math.random() * 7) + 1);
+            };
+            gameBoard.board[computerPosition] = computer.choice;
+            console.log(gameBoard.board);
+        };
+    } else {
+        alert('This field has been taken already');
+    };
     return {getWinner};
 })();
